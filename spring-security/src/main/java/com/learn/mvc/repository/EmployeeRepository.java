936e1afe2282f131a3e7bfbbee88f276f8f84918
samarth_wath <samarthwath11@gmail.com>
2025-03-03 21:54:23 +0530

    feat: Created seperate module to learn spring security.

---
+++ spring-security/src/main/java/com/learn/mvc/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.learn.mvc.repository;
+
+import com.learn.mvc.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
